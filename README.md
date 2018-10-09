1f790a3d3ea56f9aae083a47a3a0d60ed1019112
Green Man <brendan.mccaughey@gmail.com>
2018-10-09 14:10:14 -0400

    Javascript code is complete. Game functions as required.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # unit-4-game
+Escape from Crystal Cave
+
+Click on the crystals in order to reach the target number. If you reach the number without going over, you win. Otherwise, you lose.
+
+Currently Working
+==============================
+- random number chosen on game start
+- crystals assigned random values on game start
+- clicking on a crystal will add its value to the player's score
+- wins increases by one when player score matches the target number
+- losses increases by one when the player score goes over the target number
+- random number values for target score and crystals reset on win/loss
+
+Current Bugs
+=============================
+- no current bugs reported
---
+++ assets/javascript/game.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+
+    let targetNum = '';
+    let crystalA = '';
+    let crystalB = '';
+    let crystalC = '';
+    let crystalD = '';
+
+    //Counters
+    let wins = 0;
+    let losses = 0;
+    let playerScore = 0;
+
+    function reset() { //RESET
+
+        function randomNum(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min; //generates random numbers for the target and crystals
+        }
+        targetNum = randomNum(19, 120);
+        crystalA = randomNum(1, 12);
+        crystalB = randomNum(1, 12);
+        crystalC = randomNum(1, 12);
+        crystalD = randomNum(1, 12);
+
+        //player score set to 0 before each game.
+        playerScore = 0;
+        console.log('RESET');
+
+        test = false;
+        startGame();
+    }
+    startGame();
+    function startGame() {
+        function randomNum(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min; //generates random numbers for the target and crystals
+        }
+        targetNum = randomNum(19, 120);
+        crystalA = randomNum(1, 12);
+        crystalB = randomNum(1, 12);
+        crystalC = randomNum(1, 12);
+        crystalD = randomNum(1, 12);
+
+        //player score set to 0 before each game.
+        playerScore = 0;
+
+        console.log(targetNum);
+        console.log(crystalA);
+        console.log(crystalB);
+        console.log(crystalC);
+        console.log(crystalD);
+
+        $('#target').html(targetNum);
+        $('#score').html(playerScore);
+        $('#wins').html(wins);
+        $('#losses').html(losses);
+    }
+    //Win/Lose conditions.
+    function winLose() {
+        if (playerScore === targetNum) {
+            wins++;
+            reset();
+        }
+        else if (playerScore > targetNum) {
+            losses++;
+            reset();
+        }
+    }
+    //Assign buttons their crystal value and have it display as the player score on click. Additive with each button click.
+
+    $('#crystalone').click(function () {
+        playerScore = playerScore + crystalA;
+        $('#score').html(playerScore);
+        winLose();
+    });
+    $('#crystaltwo').click(function () {
+        playerScore = playerScore + crystalB;
+        $('#score').html(playerScore);
+        winLose();
+    });
+    $('#crystalthree').click(function () {
+        playerScore = playerScore + crystalC;
+        $('#score').html(playerScore);
+        winLose();
+    });
+    $('#crystalfour').click(function () {
+        playerScore = playerScore + crystalD;
+        $('#score').html(playerScore);
+        winLose();
+    });
+
+
+});
