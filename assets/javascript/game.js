f07eee1355ab5d1ca09d128756e5014b99a0d1d8
Green Man <brendan.mccaughey@gmail.com>
2018-10-13 22:52:02 -0400

    Final submission.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -108,7 +108,7 @@ $(document).ready(function () {
                     $('audio').get(7).play();
                     $('audio').get(8).play();
                     setTimeout(function () {
-                        $('#playAgainModal').modal('show');},10000);
+                        $('#playAgainModal').modal('show');},15000);
                 });
         });            
             reset();
@@ -181,124 +181,104 @@ $(document).ready(function () {
         $('#score').html('Your score: ' + playerScore);
         winLose();
     });
+
     //Fireflies
-//     'use strict';
+//     var w = window,
+//     d = document,
+//     e = d.documentElement,
+//     g = d.getElementsByTagName('body')[0],
+//     width = w.innerWidth || e.clientWidth || g.clientWidth,
+//     height = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-// var canvas = document.createElement('canvas');
-// var ctx = canvas.getContext('2d');
+// var setup = {
+//   xs: {dim: 5, weight: 41},
+//   s: {dim: 10, weight: 26},
+//   m: {dim: 50, weight: 16},
+//   l: {dim: 100, weight: 11},
+//   xl: {dim: 250, weight: 6},
+// };
 
-// var MAX_FLIES = 15;
-// var FLY_XSPEED_RANGE = [-1, 1];
-// var FLY_YSPEED_RANGE = [-0.5, 0.5];
-// var FLY_SIZE_RANGE = [1, 5];
-// var FLY_LIFESPAN_RANGE = [75, 150];
+// var numOfLights = 100;
 
-// var flies = [];
-
-// function randomRange(min, max) {
-//   return Math.random() * (max - min) + min;
+// function randomInt(max){
+//   return randomIntFromInterval(1, max);
 // }
 
-// function Fly(options) {
-//   if (!options) { options = {}; }
-
-//   this.x = options.x || randomRange(0, canvas.width);
-//   this.y = options.y || randomRange(0, canvas.height);
-//   this.xSpeed = options.xSpeed || randomRange(FLY_XSPEED_RANGE[0], FLY_XSPEED_RANGE[1]);
-//   this.ySpeed = options.ySpeed || randomRange(FLY_YSPEED_RANGE[0], FLY_YSPEED_RANGE[1]);
-//   this.size = options.size || randomRange(FLY_SIZE_RANGE[0], FLY_SIZE_RANGE[1]);
-//   this.lifeSpan = options.lifeSpan || randomRange(FLY_LIFESPAN_RANGE[0], FLY_LIFESPAN_RANGE[1]);
-//   this.age = 0;
-
-//   this.colors = options.colors || {
-//     red: 207,
-//     green: 255,
-//     blue: 4,
-//     alpha: 0
-//   };
+// function randomIntFromInterval(min,max){
+//   return Math.floor(Math.random()*(max-min+1)+min);
 // }
 
-// function fitToScreen(element) {
-//   element.width = window.innerWidth;
-//   element.height = window.innerHeight;
-// }
-
-// function clearScreen() {
-//   ctx.beginPath();
-//   ctx.fillStyle = 'rgb(0, 0, 0)';
-//   ctx.rect(0, 0, canvas.width, canvas.height);
-//   ctx.fill();
-// }
-
-// function createFlies() {
-//   if (flies.length !== MAX_FLIES) {
-//     flies.push(new Fly());
+// function randomColorRGB(){
+//   return {
+//     r: randomInt(225),
+//     g: randomInt(225),
+//     b: randomInt(225)
 //   }
 // }
 
-// function moveFlies() {
-//   flies.forEach(function(fly) {
-//     fly.x += fly.xSpeed;
-//     fly.y += fly.ySpeed;
-//     fly.age++;
+// function randomSize(){
+//   var sizes = ['xs', 's', 'm', 'l', 'xl'];
 
-//     if (fly.age < fly.lifeSpan / 2) {
-//       fly.colors.alpha += 1 / (fly.lifeSpan / 2);
+//   var rand = randomInt(100);
+//   var weightSum = 0;
 
-//       if (fly.colors.alpha > 1) { fly.colors.alpha = 1; }
-//     } else {
-//       fly.colors.alpha -= 1 / (fly.lifeSpan / 2);
+//   for (var i = 0; i < sizes.length; i++){
+//     var size = sizes[i];
+//     weightSum += setup[size].weight;
 
-//       if (fly.colors.alpha < 0) { fly.colors.alpha = 0; }
-//     }
-//   });
-// }
-
-// function removeFlies() {
-//   var i = flies.length;
-
-//   while (i--) {
-//     var fly = flies[i];
-
-//     if (fly.age >= fly.lifeSpan) {
-//       flies.splice(i, 1);
+//     if (rand <= weightSum){
+//       return size;
 //     }
 //   }
 // }
 
-// function drawFlies() {
-//   flies.forEach(function(fly) {
-//     ctx.beginPath();
-//     ctx.fillStyle = 'rgba(' + fly.colors.red + ', ' + fly.colors.green + ', ' + fly.colors.blue + ', ' + fly.colors.alpha + ')';
-//     ctx.arc(
-//       fly.x,
-//       fly.y,
-//       fly.size,
-//       0,
-//       Math.PI * 2,
-//       false
-//     );
-//     ctx.fill();
+// function Light(options) {
+//   this.size = options.size || randomSize(); // s: small, m: medium, l: large
+//   this.color = options.color || randomColor(); // object representing rgb values
+// }
+
+// Light.prototype.draw = function(container){
+//   var x = randomInt(width);
+//   var y = randomInt(height);
+//   var largerDim = width > height ? width : height;
+
+//   var lightWrapper = document.createElement('div');
+//   lightWrapper.setAttribute('class', 'lightWrapper');
+//   lightWrapper.style.left = randomInt(width) + 'px';
+//   lightWrapper.style.top = randomInt(height) + 'px';
+//   lightWrapper.style.height = lightWrapper.style.width = randomInt(largerDim) + 'px';
+//   var rotation = randomInt(360);
+//   rotation *= randomInt(2) == 1 ? 1 : -1;
+//   lightWrapper.style.transform = 'rotate(' + rotation + 'deg)';
+
+//   var lightElement = document.createElement('div');
+//   lightElement.setAttribute('class', 'light');
+//   var rgbaString = 'rgba(' + this.color.r + ',' + this.color.g + ',' + this.color.b + ',1)';
+//   lightElement.style.background = 'radial-gradient(ellipse closest-side, ' + rgbaString + ' 0%,' + rgbaString + ' ' + randomIntFromInterval(10, 75) + '%, transparent 100%)';
+//   lightElement.style.height = lightElement.style.width = setup[this.size].dim + 'px';
+//   lightElement.style.webkitAnimationDuration = randomIntFromInterval(100, 300) + 's';
+
+//   lightWrapper.appendChild(lightElement);
+//   container.appendChild(lightWrapper);
+// }
+
+// function LightFactory() {}
+// LightFactory.prototype.createLight = function () {
+//   return new Light({
+//     size: randomSize(),
+//     color: randomColorRGB()
 //   });
 // }
 
-// function render() {
-//   clearScreen();
-//   createFlies();
-//   moveFlies();
-//   removeFlies();
-//   drawFlies();
-// }
+// $('#grove').click(function() {
+//   var lightFactory = new LightFactory();
+//   var lightsContainer = document.getElementById('lights');
 
-// window.addEventListener('resize', function() {
-//   fitToScreen(canvas);
+//   for(i = 0; i < numOfLights; i++){
+//     var light = lightFactory.createLight();
+//     light.draw(lightsContainer);
+//   }
 // });
+    //End Fireflies
 
-// document.querySelector('body').appendChild(canvas);
-// fitToScreen(canvas);
-
-// (function animationLoop() {
-//   window.requestAnimationFrame(animationLoop);
-//   render();
-// })();
 });
